aa69a21d6578b5ef2d34890de5df63113acc7b8e
Colm Woods <colmwoods13@gmail.com>
2025-10-29 10:47:04 +0000

    Change folder name from accounts to account bc of allauth and now my html and css page communcating

--- gitshowcase/settings.py
+++ gitshowcase/settings.py
@@ -53,10 +53,9 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_USERNAME_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = "none"
-ACCOUNT_AUTHENTICATION_METHOD = "username_email"
+ACCOUNT_LOGIN_METHODS = {"username", "email"}
+ACCOUNT_SIGNUP_FIELDS = ["email*", "username*", "password1*", "password2*"]
+
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
