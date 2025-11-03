9276b8333495ee557eb570ed82a17fedb14b8e74
Colm Woods <colmwoods13@gmail.com>
2025-11-03 10:35:43 +0000

    Add onto my home function so it shows repos when user logged in

--- gitshowcase/views.py
+++ gitshowcase/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
+from allauth.socialaccount.models import SocialAccount, SocialToken
+import requests
 
 def home(request):
-    context = {'user': request.user}
-    return render(request, 'home.html', context)
+    repos = []
+
+    if request.user.is_authenticated:
+        try:
+            social_account = SocialAccount.objects.get(user=request.user, provider='github')
+            token = SocialToken.objects.get(account=social_account, account__user=request.user)
+            headers = {
+                'Authorization': f'token {token.token}',
+                'Accept': 'application/vnd.github+json',
+            }
+            response = requests.get('https://api.github.com/user/repos', headers=headers)
+
+            if response.status_code == 200:
+                repos = response.json()
+        except Exception as e:
+            print("GitHub API error:", e)
+
+    return render(request, 'home.html', {'repos': repos})
 
 def about(request):
     return render(request, 'about.html')
