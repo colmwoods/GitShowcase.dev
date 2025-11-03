5ff88c6d34d1cd254d1b85e3faa6071402dccc75
Colm Woods <colmwoods13@gmail.com>
2025-11-03 10:13:19 +0000

    Add def function about to views.py

--- gitshowcase/views.py
+++ gitshowcase/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def home(request):
     context = {'user': request.user}
     return render(request, 'home.html', context)
+
+def about(request):
+    return render(request, 'about.html')
