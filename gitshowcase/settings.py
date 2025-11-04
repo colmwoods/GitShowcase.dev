0b174b8b5c4fe1115242b0dab5fc6ee3a1538495
Colm Woods <colmwoods13@gmail.com>
2025-11-04 10:21:15 +0000

    Add gitshowcase as installed apps and migrated the bookmark model

--- gitshowcase/settings.py
+++ gitshowcase/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'gitshowcase',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
