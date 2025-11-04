c0dc1f044d26af942fbee3fcf2780c56ea96e243
Colm Woods <colmwoods13@gmail.com>
2025-11-04 13:05:24 +0000

    Add the model for comment in models.py

--- gitshowcase/models.py
+++ gitshowcase/models.py
@@ -10,3 +10,17 @@ class Bookmark(models.Model):
 
     def __str__(self):
         return f"{self.user.username} bookmarked {self.repo_name}"
+    
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    repo_name = models.CharField(max_length=255)
+    repo_url = models.URLField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.repo_name}"
