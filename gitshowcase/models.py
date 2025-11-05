50085841b86d0215de85b14dab078cd34a37d9ea
Colm Woods <colmwoods13@gmail.com>
2025-11-05 12:24:35 +0000

    Edit the bookmark mode so includes stars and forks

--- gitshowcase/models.py
+++ gitshowcase/models.py
@@ -5,6 +5,10 @@ class Bookmark(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     repo_name = models.CharField(max_length=255)
     repo_url = models.URLField()
+    language = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    stargazers_count = models.IntegerField(default=0)
+    forks_count = models.IntegerField(default=0)
     notes = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
