95a9402f21b54c1ea8d6bfbecff9f44d99077d11
Colm Woods <colmwoods13@gmail.com>
2025-11-10 13:24:38 +0000

    Remove repo name as url covers it from admin.py

--- gitshowcase/admin.py
+++ gitshowcase/admin.py
@@ -1,14 +1,26 @@
 from django.contrib import admin
 from .models import Bookmark, Comment
 
+
 @admin.register(Bookmark)
 class BookmarkAdmin(admin.ModelAdmin):
-    list_display = ("user", "repo_name", "repo_url", "language", "created_at")
-    search_fields = ("repo_name", "repo_url", "language")
-    list_filter = ("language", "created_at")
+    list_display = ("user", "repo_link", "created_at")
+    search_fields = ("repo_url", "user__username")
+    fields = ("user", "repo_url")  # Only show these in Add/Edit form
+
+    def repo_link(self, obj):
+        return f'<a href="{obj.repo_url}" target="_blank">{obj.repo_url}</a>'
+    repo_link.allow_tags = True
+    repo_link.short_description = "Repository URL"
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ("user", "repo_name", "body", "created_at")
-    search_fields = ("repo_name", "body", "user__username")
-    list_filter = ("created_at",)
+    list_display = ("user", "repo_link", "body", "created_at")
+    search_fields = ("repo_url", "user__username", "body")
+    fields = ("user", "repo_url", "body")  # Only show these in Add/Edit form
+
+    def repo_link(self, obj):
+        return f'<a href="{obj.repo_url}" target="_blank">{obj.repo_url}</a>'
+    repo_link.allow_tags = True
+    repo_link.short_description = "Repository URL"
