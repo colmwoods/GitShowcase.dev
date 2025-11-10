bc01ab110f1cb9d9cd257a20c10c7260a87bd771
Colm Woods <colmwoods13@gmail.com>
2025-11-10 10:26:00 +0000

    Change DEBUG to False so 404 page will load

--- gitshowcase/settings.py
+++ gitshowcase/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get(
     'django-insecure-2aijh9qp!r_sems1v=lt*2jh7s&o-2-g7ewaspe!t_@sna@o1$'
 )
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
