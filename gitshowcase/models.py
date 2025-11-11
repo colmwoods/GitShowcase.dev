c92504f1ee8b32ac3632026f098cda92a7afdd7f
Colm Woods <colmwoods13@gmail.com>
2025-11-11 12:30:18 +0000

    Add ContactMessage model in models.py

--- gitshowcase/models.py
+++ gitshowcase/models.py
@@ -28,3 +28,14 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.user.username} on {self.repo_name}"
+
+class ContactMessage(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20, blank=True)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} — {self.email}"
