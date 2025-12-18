b9e01280ec453aa0cd98b801956d33d3f7a687f5
Colm Woods <colmwoods13@gmail.com>
2025-12-18 11:15:43 +0000

    Change name of page from login failed to oauth failed and ran autopep8 to fix views.py

--- gitshowcase/views.py
+++ gitshowcase/views.py
@@ -512,5 +512,9 @@ def success(request):
     """
     return render(request, 'form/success.html')
 
+
 def oauth_callback_guard(request):
+    """
+    Guard for OAuth callback cancellations.
+    """
     return redirect("/accounts/3rdparty/login/cancelled/")
